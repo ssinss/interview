4c8504151235299628685b6d14061f622a63e780
WoongBi, Kim <ssinss@gmail.com>
2020-08-05 12:28:33 +0900

    첫 커밋!

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+const cons = [
+  {
+    brand: '이디야',
+    sellCount: 30,
+    name: '아메리카노',
+  },
+  {
+    brand: 'CU',
+    sellCount: 20,
+    name: '1만원권',
+  },
+
+  {
+    brand: '스타벅스',
+    sellCount: 7,
+    name: '카페모카',
+  },
+  {
+    brand: '이디야',
+    sellCount: 3,
+    name: '카페모카',
+  },
+  {
+    brand: '스타벅스',
+    sellCount: 10,
+    name: '핫초코',
+  },
+  {
+    brand: '스타벅스',
+    sellCount: 11,
+    name: '카페라떼',
+  },
+  {
+    brand: 'CU',
+    sellCount: 6,
+    name: '1천원권',
+  },
+  {
+    brand: 'GS25',
+    sellCount: 93,
+    name: '1만원권',
+  },
+  {
+    brand: 'GS25',
+    sellCount: 21,
+    name: '1천원권',
+  },
+  {
+    brand: '스타벅스',
+    sellCount: 70,
+    name: '아메리카노',
+  },
+]
+
+// 함수 형
+const App = () => {
+  return <div></div>
+}
+
+// 클래스형 컴포넌트로 작성을 원하면 아래 폼을 이용해 주세요.
+//
+// class App extends Component {
+//   render() {
+//     return <div></div>
+//   }
+// }
+
+export default App
